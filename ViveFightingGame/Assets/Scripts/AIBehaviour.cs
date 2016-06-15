873eb71e479196a669452ac6e35ef7703102c201
Jordy <jordywollaars@kpnmail.nl>
2016-06-15 22:55:32 +0200

    zombies+spawners+Environment

--- ViveFightingGame/Assets/Scripts/AIBehaviour.cs
+++ ViveFightingGame/Assets/Scripts/AIBehaviour.cs
@@ -5,14 +5,21 @@ public class AIBehaviour : MonoBehaviour
 {
     private NavMeshAgent m_NMA;
 
+    private GameObject m_Player;
+
+    [SerializeField]
+    private float m_ZombieSpeed = 0;
+
     void Start()
     {
-
+        m_NMA = GetComponent<NavMeshAgent>();
+        m_Player = GameObject.Find("PlayerLoc");
+        m_NMA.SetDestination(m_Player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        m_NMA.speed = m_ZombieSpeed;
     }
 }
---
+++ ViveFightingGame/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float m_SpawnSpeed;
+    [SerializeField]
+    private float m_Timer;
+    [SerializeField]
+    private GameObject m_PrefabToSpawn;
+    [SerializeField]
+    private GameObject[] m_SpawnPoints;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        m_Timer -= Time.deltaTime;
+        if(m_Timer <= 0)
+        {
+            int spawnPos = Random.Range(0, m_SpawnPoints.Length);
+            GameObject zombie = Instantiate(m_PrefabToSpawn, m_SpawnPoints[spawnPos].transform.position, Quaternion.identity) as GameObject;
+            m_Timer = m_SpawnSpeed;
+        }
+    }
+}
