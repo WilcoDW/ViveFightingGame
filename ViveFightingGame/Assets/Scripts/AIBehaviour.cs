5ecd0909204b9e0b83277c67296ea99b46093cbb
Jordy <jordywollaars@kpnmail.nl>
2016-06-15 13:09:57 +0200

    New assets
    > skybox
    > fireflies particle
    > mirror
    > trees + house

---
+++ ViveFightingGame/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIBehaviour : MonoBehaviour
+{
+    private NavMeshAgent m_NMA;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
